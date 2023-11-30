4d1e52b985aa35440994efe6244caa57a50829d1
Leandro <ramosdossantos.leandro@gmail.com>
2023-11-30 20:44:41 -0300

    Index route update

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [BookController::class, 'index']);
 
 Route::resource('/books',BookController::class);
 
