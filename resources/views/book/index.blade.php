aa8f1d1f9316dd613d1f81b960c5e086b2a0efa2
Leandro <ramosdossantos.leandro@gmail.com>
2023-12-03 03:36:00 -0300

    Show book tags

--- resources/views/book/index.blade.php
+++ resources/views/book/index.blade.php
@@ -7,7 +7,7 @@
 
 <body>
     <div class="p-2 w-full">
-        <div class="grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
             @forelse ($books as $book)
                 <div class="flex items-center justify-center ">
                     <div class="w-fit text-center">
